64c0aa69a8098121b67fadf7a039c61a27b96f08
Rik de Goede <rik.dgd@gmail.com>
2022-05-11 14:20:17 +0200

    more test progress

--- src/test/java/com/pokemoncardmarkt/pokemoncardmarkt_backend/PokemonCardMarktBackEndApplicationTests.java
+++ src/test/java/com/pokemoncardmarkt/pokemoncardmarkt_backend/PokemonCardMarktBackEndApplicationTests.java
@@ -1,12 +1,17 @@
 package com.pokemoncardmarkt.pokemoncardmarkt_backend;
 
 import com.pokemoncardmarkt.pokemoncardmarkt_backend.controller.CardController;
+import com.pokemoncardmarkt.pokemoncardmarkt_backend.model.Expansion;
+import com.pokemoncardmarkt.pokemoncardmarkt_backend.model.PokemonCard;
 import com.pokemoncardmarkt.pokemoncardmarkt_backend.repository.PokemonCardRepository;
 import com.pokemoncardmarkt.pokemoncardmarkt_backend.services.CardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class PokemonCardMarktBackEndApplicationTests {
@@ -15,11 +20,16 @@ class PokemonCardMarktBackEndApplicationTests {
     void GetCardTest() {
         // arrange
         PokemonCardRepository cardRepo = mock(PokemonCardRepository.class);
-        CardService cardService = new CardService();
+        CardService cardService = new CardService(cardRepo);
         CardController controller = new CardController(cardService);
+        PokemonCard foundCard;
+
+        Expansion expansion = new Expansion();
+        PokemonCard definedCard = new PokemonCard(1, "Pikachu", 20, PokemonTypes.electric, expansion);
+
 
         // act
-
+        when(cardRepo.findById(1L)).thenReturn(Optional.of(definedCard));
 
         // assert
 
